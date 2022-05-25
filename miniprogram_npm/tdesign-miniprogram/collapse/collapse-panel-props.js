43e9aa2e5cbf6a1e261d9a221c363c1275770249
youlan_lan <youlan_lan@qq.com>
2022-05-26 01:02:32 +0800

    chore: 初始化项目

---
+++ miniprogram_npm/tdesign-miniprogram/collapse/collapse-panel-props.js
@@ -0,0 +1,24 @@
+const props = {
+    content: {
+        type: String,
+    },
+    disabled: {
+        type: Boolean,
+        value: null,
+    },
+    expandIcon: {
+        type: Boolean,
+        value: null,
+    },
+    header: {
+        type: String,
+    },
+    headerRightContent: {
+        type: String,
+    },
+    value: {
+        type: String,
+        optionalTypes: [Number],
+    },
+};
+export default props;
